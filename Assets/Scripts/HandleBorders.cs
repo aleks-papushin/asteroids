a3f29bda7903500daf2ca2fbf689c0a83a142d5a
Aleksey Papushin <aleks.papushin@gmail.com>
2020-03-23 21:07:50 +0300

    Added player script and sprite. Implemented movement and teleporting at sceen borders

---
+++ Assets/Scripts/HandleBorders.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class HandleBorders : MonoBehaviour
+{
+    Camera cam;
+    float horizontalBound;
+    float verticalBound;
+    Vector3 screenBorders;
+
+    void Start()
+    {
+        cam = Camera.main;
+        // gets position of camera right top point 
+        screenBorders = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        horizontalBound = screenBorders.x;
+        verticalBound = screenBorders.y;
+    }
+
+    void Update()
+    {
+        if (transform.position.x > horizontalBound)
+        {
+            transform.position = new Vector3(-horizontalBound, transform.position.y, 0);
+        }
+        else if (transform.position.x < -horizontalBound)
+        {
+            transform.position = new Vector3(horizontalBound, transform.position.y, 0);
+        }
+
+        else if (transform.position.y > verticalBound)
+        {
+            transform.position = new Vector3(transform.position.x, -verticalBound, 0);
+        }
+
+        else if (transform.position.y < -verticalBound)
+        {
+            transform.position = new Vector3(transform.position.x, verticalBound, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float rotationSpeed = 200.0f;
+    float engineForce = 2f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
+
+        GetComponent<Rigidbody2D>().AddForce(transform.up * engineForce * Input.GetAxis("Vertical"));
+    }
+}
