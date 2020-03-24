6d20198740ae69e5194dc83dff13d307d4bed85a
Aleksey Papushin <aleks.papushin@gmail.com>
2020-03-25 01:59:56 +0300

    Added asteroid destruction by collision with projectile

---
+++ Assets/Scripts/DestroyOnCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class DestroyOnCollision : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Asteroid"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
