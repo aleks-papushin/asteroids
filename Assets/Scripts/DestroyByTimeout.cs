e63fee50d9cc578805f4b045fb6d5c161c890bef
Aleksey Papushin <aleks.papushin@gmail.com>
2020-03-25 00:29:13 +0300

    Added shooting method, projectile and according scripts

---
+++ Assets/Scripts/DestroyByTimeout.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DestroyByTimeout : MonoBehaviour
+{
+    public float timeoutSeconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitTimeout());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator WaitTimeout()
+    {
+        yield return new WaitForSeconds(timeoutSeconds);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject projectile;
+
     float rotationSpeed = 200.0f;
     float engineForce = 2f;
 
@@ -15,5 +17,15 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
 
         GetComponent<Rigidbody2D>().AddForce(transform.up * engineForce * Input.GetAxis("Vertical"));
+
+        HandleShooting();
+    }
+
+    void HandleShooting()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectile, transform.position, transform.rotation);
+        }
     }
 }
