b4ce5d1415a16c2d3f7e991d0c65c212e4bd414f
Aleksey Papushin <aleks.papushin@gmail.com>
2020-03-25 01:23:52 +0300

    Added asteriod and moving script

---
+++ Assets/Scripts/MovingRandomly.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MovingRandomly : MonoBehaviour
+{
+    public float speed;    
+    public float rotatingSpeed;
+
+    Vector3 movement;
+    float rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = new Vector3(Random.Range(-speed, speed), Random.Range(-speed, speed));
+        rotation = Random.Range(-rotatingSpeed, rotatingSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(movement * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.forward, rotation * Time.deltaTime);
+    }
+}
