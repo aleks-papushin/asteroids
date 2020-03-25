3d71e611b9cfee72f152536d2c26ab28e2d22d6e
Aleksey Papushin <aleks.papushin@gmail.com>
2020-03-26 00:04:55 +0300

    Added shooting script for big ufo

---
+++ Assets/Scripts/ShootingRandomly.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ShootingRandomly : MonoBehaviour
+{
+    public GameObject ufoProjectile;
+
+    public float speed;
+    public float timeoutSeconds;
+
+    bool shouldShoot = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleShooting();
+    }
+
+    private void HandleShooting()
+    {
+        if (shouldShoot)
+        {
+            StartCoroutine(ShootAndWaitCooldown());
+            shouldShoot = false;
+        }
+    }
+
+    private IEnumerator ShootAndWaitCooldown()
+    {
+        Instantiate(ufoProjectile, transform.position, transform.rotation);
+        yield return new WaitForSeconds(timeoutSeconds);
+        shouldShoot = true;
+    }
+}
---
+++ Assets/Scripts/UfoProjectileRandomMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class UfoProjectileRandomMovement : MonoBehaviour
+{
+    public float speed;
+
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        movement.Normalize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
