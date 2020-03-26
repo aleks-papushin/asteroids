4e9a6315075c6be615caf965c896d080b5fb9b7b
Aleksey Papushin <aleks.papushin@gmail.com>
2020-03-26 13:34:48 +0300

    added scripts for manage ufo behavior

---
+++ Assets/Scripts/SpawnUfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpawnUfo : MonoBehaviour
+{
+    public GameObject bigUfo;
+    public float bigUfoSpawningTimeout;
+
+    GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        StartCoroutine(SpawnBig());        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnBig()
+    {
+        yield return new WaitForSeconds(bigUfoSpawningTimeout);
+        Instantiate(bigUfo, GetRandomUfoPosition(), bigUfo.transform.rotation);
+    }
+
+    Vector2 GetRandomUfoPosition()
+    {
+        // for random picking left or right screen side        
+        var xModifiers = new int[] { -1, 1 };
+        var xMod = xModifiers[Random.Range(0, xModifiers.Length)];
+
+        // generate position with x as left or right border and random y position
+        var position = new Vector2(
+            gameManager.horizontalBound * xMod, 
+            Random.Range(0f, gameManager.verticalBound));
+
+        return position;
+    }
+}
---
+++ Assets/Scripts/UfoMoving.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class UfoMoving : MonoBehaviour
+{
+    public float speed;
+    public float changingDirectionTimeout;
+    bool shouldChangeDirection = false;
+
+    int movementXDirection;
+    float[] yAngles = new float[] { -0.5f, 0, 0.5f };
+    Vector2 movement;
+    float xMovement;
+    float yMovement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // choose direction opposite to side you've appeared    
+        movementXDirection = transform.position.x < 0 ? 1 : -1;
+        xMovement = 0.5f * movementXDirection;        
+        CalculateMovement();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {   
+        transform.Translate(movement * speed * Time.deltaTime);
+
+        if (shouldChangeDirection)
+        {
+            CalculateMovement();            
+        }
+    }
+
+    // accidentally move by 45 degree for a a couple of seconds, than move horizontally again
+    private IEnumerator WaitTimeout()
+    {
+        yield return new WaitForSeconds(changingDirectionTimeout);
+        shouldChangeDirection = true;
+    }
+
+    private void CalculateMovement()
+    {
+        yMovement = yAngles[Random.Range(0, yAngles.Length)];
+        movement = new Vector2(xMovement, yMovement);
+        movement.Normalize();
+        shouldChangeDirection = false;
+        StartCoroutine(WaitTimeout());
+    }
+}
