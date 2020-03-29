1d2481c966776306e7f3c48fa89000a4c0a5d2b2
Aleksey Papushin <aleks.papushin@gmail.com>
2020-03-29 19:04:27 +0300

    Added engine fire blinking feature

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,8 @@ public class PlayerController : MonoBehaviour
 
     GameManager gameManager;
     Rigidbody2D rig;
-    SpriteRenderer sprite;
+    public SpriteRenderer shipSprite;
+    public SpriteRenderer fireSprite;
 
     const float rotationSpeed = 200.0f;
     const float engineForce = 2f;
@@ -16,15 +17,22 @@ public class PlayerController : MonoBehaviour
     bool canTeleport = true;
 
     const float invincibilityTimeout = 3;
-    float blinkingTimer = 0;
-    const float blinkingInterval = 0.15f;
+    float shipBlinkingTimer = 0;
+    const float shipBlinkingInterval = 0.15f;
     bool isInvincible = false;
+
+    float fireBlinkingTimer = 0;
+    float fireBlinkingInterval = 0.04f;
+    bool isShowEngineFire = false;
 
     void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
         rig = GetComponent<Rigidbody2D>();
-        sprite = GetComponent<SpriteRenderer>();
+        shipSprite = GetComponent<SpriteRenderer>();
+        fireSprite = transform.Find("Fire").GetComponent<SpriteRenderer>();
+
+        StartCoroutine(HandleEngineFire());
     }
 
     void Update()
@@ -34,11 +42,59 @@ public class PlayerController : MonoBehaviour
         HandleTeleport();
     }
 
+    public IEnumerator Teleport(bool isLifeLost = false)
+    {
+        canTeleport = false;
+
+        // disappear player
+        GetComponent<Collider2D>().enabled = false;
+        shipSprite.enabled = false;
+
+        // wait for timeout
+        yield return new WaitForSeconds(timeoutBeforeTeleporting);
+
+        // appear at new pos
+        if (isLifeLost)
+        {
+            isInvincible = true;
+            transform.position = Vector2.zero;
+            transform.rotation = Quaternion.identity;
+        }
+        else // if just teleporting
+        {
+            var newPos = gameManager.GetRandomPosition();
+            transform.position = newPos;
+        }
+
+        // stop movement
+        rig.velocity = Vector2.zero;
+        rig.angularVelocity = 0f;
+
+        // show player, handle invincibility (be invincible if life was lost)
+        if (isInvincible)
+        {
+            StartCoroutine(BeInvincible());
+        }
+        else
+        {
+            shipSprite.enabled = true;
+            GetComponent<Collider2D>().enabled = true;
+
+            canTeleport = true;
+        }
+    }
+
     private void HandleMoving()
     {
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
             rig.AddForce(transform.up * engineForce);
+            isShowEngineFire = true;            
+        }
+        else if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W))
+        {
+            isShowEngineFire = false;
+            fireSprite.enabled = false;
         }
 
         transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
@@ -60,71 +116,29 @@ public class PlayerController : MonoBehaviour
             Input.GetKeyDown(KeyCode.DownArrow);
     }
 
-    public IEnumerator Teleport(bool isLifeLost = false)
-    {
-        canTeleport = false;
-
-        // disappear player
-        GetComponent<Collider2D>().enabled = false;
-        sprite.enabled = false;
-
-        // wait for timeout
-        yield return new WaitForSeconds(timeoutBeforeTeleporting);
-
-        // appear at new pos
-        if (isLifeLost) 
-        {
-            isInvincible = true;
-            transform.position = Vector2.zero;
-            transform.rotation = Quaternion.identity;
-        }
-        else // if just teleporting
-        {
-            var newPos = gameManager.GetRandomPosition();
-            transform.position = newPos;
-        }
-
-        // stop movement
-        rig.velocity = Vector2.zero;
-        rig.angularVelocity = 0f;
-
-        // show player, handle invincibility (be invincible if life was lost)
-        if (isInvincible)
-        {        
-            StartCoroutine(BeInvincible());
-        }
-        else
-        {
-            sprite.enabled = true;
-            GetComponent<Collider2D>().enabled = true;
-
-            canTeleport = true;
-        }
-    }
-
     private IEnumerator BeInvincible()
     {
-        StartCoroutine(SpriteBlink());
+        StartCoroutine(ShipSpriteBlink());
 
         yield return new WaitForSeconds(invincibilityTimeout);
 
         isInvincible = false;
 
-        sprite.enabled = true;
+        shipSprite.enabled = true;
         GetComponent<Collider2D>().enabled = true;
                 
         canTeleport = true;
     }
 
-    private IEnumerator SpriteBlink()
+    private IEnumerator ShipSpriteBlink()
     {
         while (isInvincible)
         {
-            blinkingTimer += Time.deltaTime;
-            if (blinkingTimer > blinkingInterval)
+            shipBlinkingTimer += Time.deltaTime;
+            if (shipBlinkingTimer > shipBlinkingInterval)
             {
-                sprite.enabled = !sprite.enabled;
-                blinkingTimer = 0;
+                shipSprite.enabled = !shipSprite.enabled;
+                shipBlinkingTimer = 0;
             }
 
             yield return null;
@@ -138,6 +152,27 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(projectile, transform.position, transform.rotation);
+        }
+    }
+
+    private IEnumerator HandleEngineFire()
+    {
+        while (true)
+        {
+            while (isShowEngineFire)
+            {
+                Debug.Log($"Inside while loop of HandleEngineFire. isShowEngineFire: {isShowEngineFire}");
+
+                fireBlinkingTimer += Time.deltaTime;
+                if (fireBlinkingTimer > fireBlinkingInterval)
+                {
+                    fireSprite.enabled = !fireSprite.enabled;
+                    fireBlinkingTimer = 0;
+                }
+
+                yield return null;
+            }
+            yield return null;
         }
     }
 }
