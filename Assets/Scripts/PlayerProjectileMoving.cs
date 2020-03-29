9087fe7cae835e46ebad0b22702b07d1ddd34537
Aleksey Papushin <aleks.papushin@gmail.com>
2020-03-29 15:20:48 +0300

    Added player velocity magnitude to player projectile speed

---
+++ Assets/Scripts/PlayerProjectileMoving.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerProjectileMoving : MonoBehaviour
+{
+    GameObject player;
+
+    public float speed;
+    public float playerVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerVelocity = player.GetComponent<Rigidbody2D>().velocity.magnitude;
+        speed += playerVelocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+}
